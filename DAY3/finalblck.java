39b058bfaea673b865494fb6f9f30eabc8e47403
Yashwanth S K <yashutilak@gmail.com>
2026-08-19 09:14:59 +0000

    uploaded

---
+++ DAY3/finalblck.java
@@ -0,0 +1,20 @@
+package DAY3;
+
+public class finalblck {
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+		int c=10/0;
+		try {
+			System.out.println(10/0);
+		}
+		catch(NullPointerException e) {
+			System.out.println("NP exception Handled");
+		}
+		finally {
+			System.out.println("fianlly block Will execute always");
+		}
+		System.out.println("rest of code");
+	}
+
+}
